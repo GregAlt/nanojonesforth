9f0fc2d199ae1843d04bfbac6fad43232340f17c
GregAlt <greg.alt@gmail.com>
2026-02-19 19:24:48 -0800

    moved more defcode words to nanojonesforthcore.f. 38 remaining

--- nanojonesforthcore.f
+++ nanojonesforthcore.f
@@ -1,6 +1,9 @@
 : INVERT DUP NAND ;
 : AND NAND INVERT ;
 : 2DROP DROP DROP ;
+: RDROP RSP@ @ RSP@ 4 + RSP! RSP@ ! ;
+: >R RSP@ @ SWAP RSP@ ! RSP@ 4 - DUP RSP! ! ;
+: R> RSP@ 4+ @ RSP@ @ RSP@ 4+ ! RSP@ 4+ RSP! ;
 : ROT >R SWAP R> SWAP ;
 : -ROT ROT ROT ;
 : 2SWAP ROT >R ROT R> ;
@@ -21,6 +24,23 @@
 : 0>= 0< 0= ;
 : OR INVERT SWAP INVERT NAND ;
 : XOR 2DUP NAND DUP -ROT NAND -ROT NAND NAND ;
+: IMMEDIATE LATEST @ 4 + DUP C@ DUP F_IMMED SWAP F_IMMED NAND DUP NAND - SWAP F_IMMED DUP NAND
+    NAND DUP NAND + SWAP C! ; IMMEDIATE
 : IF IMMEDIATE ' 0BRANCH , HERE @ 0 , ;
 : THEN IMMEDIATE DUP HERE @ SWAP - SWAP ! ;
 : ?DUP DSP@ @ IF DUP THEN ;
+: +! 2DUP @ + SWAP ! DROP ;
+: -! DUP -ROT @ - SWAP ! ;
+: SYSCALL2 0 SWAP 2SWAP ROT SYSCALL3 ;
+: SYSCALL1 0 -ROT SYSCALL2 ;
+: SYSCALL0 0 SWAP SYSCALL1 ;
+: TELL SWAP 1 SYS_WRITE SYSCALL3 DROP ;
+: LITSTRING
+        RSP@ @ DUP 2DUP         \ next next next next(top)
+        @ 4+ 3 +                \ next next next len+4+3
+        -4 AND                  \ next next next (len+4+3)&~3
+        +                       \ next next next+(len+4+3)&~3
+        RSP@ !                  \ next next [next += (len+4+3)&~3]
+        4+ SWAP                 \ next+4 next
+        @                       \ next+4 len
+;
